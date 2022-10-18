0a6d6e571d193cc414a7ae3596a53358b8411c2c
KartikBelligatti <belligattikartik17@gmail.com>
2022-10-18 23:47:30 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
  
-  userapi:"https://heroku-json-server-samsung.herokuapp.com/users",
+  userapi:"https://localhost:7286/api/Registrations",
   cartapi:"https://heroku-json-server-samsung.herokuapp.com/cart",
   postsapi:"https://heroku-json-server-samsung.herokuapp.com/posts"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: true,
-  userapi:"https://heroku-json-server-samsung.herokuapp.com/users",
+  // userapi:"https://heroku-json-server-samsung.herokuapp.com/users",
+  userapi:"https://localhost:7286/api/Registrations",
   cartapi:"https://heroku-json-server-samsung.herokuapp.com/cart",
   postsapi:"https://heroku-json-server-samsung.herokuapp.com/posts"
   // userapi:"https://fake-server-app-angular-amazon.herokuapp.com/users",
